a67ea17aa79e62e605b736e557406ae4fcef5002
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-07-12 00:26:11 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -43,8 +43,7 @@ end
 
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
 def number_ingredients(perfect_10_recipe)
-  perfect_10_recipe.each do |ingredient|
-    puts 
+  perfect_10_recipe.length
 end
 end
 end
